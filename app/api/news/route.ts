472f338ca4eadcd2fd0fd78c4c3a87908b5dace7
CB <cbshrestha24@gmail.com>
2024-03-04 23:53:27 +1100

    removed api

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,16 +1,8 @@
 import { NextResponse } from "next/server"
-import NewsAPI  from "newsapi"
+
 
 export async function GET(){
-    const newsapi = new NewsAPI("2599f3f0235244f29c87365e86c5fa47")
-
-        const allNews = await newsapi.v2.topHeadlines({
-            sources: 'bbc-news,the-verge',
-            q: 'bitcoin',
-            category: 'business',
-            language: 'en',
-            country: 'au'
-        })
+  
 
         return NextResponse.json({hello:"world"})
 }
